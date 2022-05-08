f128f7ea2b841dcfc6b0bde5c54f81966bdfaf78
Christoffer Giertsen <chrgie90@gmail.com>
2022-05-08 22:07:21 +0200

    final

--- holidaze/src/components/layout/InfoCards.js
+++ holidaze/src/components/layout/InfoCards.js
@@ -1,33 +1,58 @@
 import { Row, Card, Col, Container } from "react-bootstrap";
 import Heading from "./Heading";
+import { BASE_URL } from "../../constants/api";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const url = BASE_URL + `/api/infocards?populate=*`;
 
 export default function InfoCards() {
+  const [info, setInfo] = useState([]);
+  // eslint-disable-next-line
+  const [loading, setLoading] = useState(true);
+  // eslint-disable-next-line
+  const [error, setError] = useState(null);
+
+  useEffect(function () {
+    async function getinfo() {
+      try {
+        const response = await axios.get(url);
+        console.log("response", response.data.data);
+
+        setInfo(response.data.data);
+      } catch (error) {
+        console.log(error);
+        setError(error.toString());
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    getinfo();
+  }, []);
+
   return (
     <Container className="text-center card-section">
-      <Heading content="Did you know this about Bergen?" />
+      <Heading content="Did you know..." />
 
       <Row xs={1} md={3} className="g-4">
-        {Array.from({ length: 3 }).map((_, idx) => (
-          <Col key={idx}>
-            <Card>
-              <Card.Img
-                variant="top"
-                src="https://dummyimage.com/300x200/000/fff"
-              />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  lorem ipsum dolar tollar fjollar lorem ipsum dolar tollar
-                  fjollar lorem ipsum dolar tollar fjollar lorem ipsum dolar
-                  tollar fjollar lorem ipsum dolar tollar fjollar lorem ipsum
-                  dolar tollar fjollar lorem ipsum dolar tollar fjollar lorem
-                  ipsum dolar tollar fjollar lorem ipsum dolar tollar fjollar
-                  lorem ipsum dolar tollar fjollar
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
+        {info.map((data) => {
+          return (
+            <Col key={data.id}>
+              <Card>
+                <Card.Img
+                  variant="top"
+                  alt={data.attributes.name}
+                  src={BASE_URL + data.attributes.img.data.attributes.url}
+                />
+                <Card.Body>
+                  <Card.Title>{data.attributes.name}</Card.Title>
+                  <Card.Text>{data.attributes.text}</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          );
+        })}
       </Row>
     </Container>
   );
